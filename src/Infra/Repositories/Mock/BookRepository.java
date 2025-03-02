c9631c70eb9cfea7ede588f7430db7329ffab1e3
wendellunny <wendelferreiraportugal@gmail.com>
2025-03-01 23:51:46 -0300

    feat: create concrete book repository mock

---
+++ src/Infra/Repositories/Mock/BookRepository.java
@@ -0,0 +1,30 @@
+package Infra.Repositories.Mock;
+
+import Domain.Entities.Bible;
+import Domain.Entities.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BookRepository implements Domain.Api.Repositories.BookRepository {
+
+    /**
+     * Fetch all books from bible
+     *
+     * @param bible Bible
+     * @return Book[]
+     */
+    @Override
+    public List<Book> fetchByBible(Bible bible)
+    {
+        List<Book> books = new ArrayList<Book>();
+
+        books.add(new Book(1, "Gênesis"));
+        books.add(new Book(2, "Êxodo"));
+        books.add(new Book(3, "Levítico"));
+        books.add(new Book(4, "Números"));
+        books.add(new Book(5, "Deutoronômio"));
+
+        return books;
+    }
+}
