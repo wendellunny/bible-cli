a5ce98d1da1fbf33d5e6fa668434374e2d79d5e2
wendellunny <wendelferreiraportugal@gmail.com>
2025-03-01 21:16:18 -0300

    feat: start app and dependency injection in main component

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
+import App.AbstractApp;
+import App.Cli.Command.ListBiblesOnScreen;
+import Domain.UseCases.ListBibles;
+import Infra.Repositories.Mock.BibleRepository;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Domain.Api.Repositories.BibleRepository bibleRepository = new BibleRepository();
+        ListBibles listBibles = new ListBibles(bibleRepository);
+        ListBiblesOnScreen listBiblesOnScreen = new ListBiblesOnScreen(
+            listBibles
+        );
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        AbstractApp app = new App.Cli.Bootstrap.App(
+            listBiblesOnScreen
+        );
+
+        app.run();
     }
 }
