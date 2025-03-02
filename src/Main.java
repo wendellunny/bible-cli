891e1425c266eab4362de50b2734b4d7ff0aebd9
wendellunny <wendelferreiraportugal@gmail.com>
2025-03-02 00:10:05 -0300

    feat: List Books Dependency Injection in Main Component

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import App.AbstractApp;
 import App.Cli.Command.ListBiblesOnScreen;
+import App.Cli.Command.ListBooksOnScreen;
 import App.Cli.Command.ShowBibleOnScreen;
 import Domain.UseCases.AccessBible;
 import Domain.UseCases.ListBibles;
+import Domain.UseCases.ListBooks;
 import Infra.Repositories.Mock.BibleRepository;
+import Infra.Repositories.Mock.BookRepository;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -18,9 +21,14 @@ public class Main {
         AccessBible accessBible = new AccessBible(bibleRepository);
         ShowBibleOnScreen showBibleOnScreen = new ShowBibleOnScreen(accessBible);
 
+        Domain.Api.Repositories.BookRepository bookRepository = new BookRepository();
+        ListBooks listBooks = new ListBooks(bookRepository);
+        ListBooksOnScreen listBooksOnScreen = new ListBooksOnScreen(listBooks);
+
         AbstractApp app = new App.Cli.Bootstrap.App(
             listBiblesOnScreen,
-            showBibleOnScreen
+            showBibleOnScreen,
+            listBooksOnScreen
         );
 
         app.run();
